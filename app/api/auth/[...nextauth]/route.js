0f0307f27c91a9be7dbe959967f77bff1d6add23
Christian <chrtaw@outlook.com>
2023-10-21 21:40:45 -0600

    mongoDB connect

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,11 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { connectToDB } from "@utils/database";
 
-cosole.log({
-  clientId: process.env.GOOGLE_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-
-})
 
 const handler = NextAuth({
   providers: [
@@ -14,7 +10,23 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {},
-  async signIn({ profile }) {},
+  async session({ session }) {
+
+  },
+  async signIn({ profile }) {
+    try{
+      //serverLess -> Lambda -> dynamodb
+      await connectToDB();
+      // check if a user already exist
+
+      // if not, create a new user
+
+      return true;
+    } catch (error){
+      console.log(error);
+      return false
+    }
+   
+  },
 });
 export { handler as GET, handler as POST };
