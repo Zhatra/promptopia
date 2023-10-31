428eb07c5cde5c46663f3e9b8a0e4772fbfd685a
Christian <chrtaw@outlook.com>
2023-10-31 02:24:43 -0600

    next auth and new content in next.config

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { connectToDB } from "@utils/database";
 
+import User from '@models/user';
 
 const handler = NextAuth({
   providers: [
@@ -11,15 +12,30 @@ const handler = NextAuth({
     }),
   ],
   async session({ session }) {
+    const sessionUser = await User.findOne({
+      email: session.user.email
+    })
 
+    session.user.id = sessionUser._id.toString();
+
+    return session;
   },
   async signIn({ profile }) {
     try{
       //serverLess -> Lambda -> dynamodb
       await connectToDB();
       // check if a user already exist
-
+      const userExist = await User.findOne({
+        email: profile.email
+      })
       // if not, create a new user
+      if(!userExist){
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace("","").toLowerCase(),
+          image: profile.picture
+        })
+      }
 
       return true;
     } catch (error){
