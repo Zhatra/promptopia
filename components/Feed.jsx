59b0262299dca08ac1053cca5e7ad783773ceda1
Christian <chrtaw@outlook.com>
2023-11-16 23:51:42 -0600

    feed and promptcard

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+'use client';
+import {useState,useEffect} from 'react'
+import PromptCard from './PromptCard';
+
+const PromptCardList = ({data, handleTagClick})=>{
+  return(
+    <div className=''>
+     
+    </div>
+  )
+}
+
 
 function Feed() {
+  const [searchText, setSearchText] = useState('');
+
+  const handleSearchChange = (e) => {
+
+  }
+
   return (
-    <div>Feed jsj</div>
+    <section className='feed'>
+      <form className='relative w-full flex-center'>
+        <input 
+          type="text"
+          placeholder='Search for a tag or a username'
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className='search_input peer'
+        />
+          
+        
+      </form>
+
+      <PromptCardList
+        data={[]}
+        handleTagClick={()=>{}}
+      
+      />
+    </section>
   )
 }
 
--- components/PromprCard.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function PromprCard() {
-  return (
-    <div>PromprCard</div>
-  )
-}
-
-export default PromprCard
