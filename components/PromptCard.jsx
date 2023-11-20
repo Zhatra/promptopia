c2c8789d53af1eef75fe92924ced0b7b7d7aea2b
Christian <chrtaw@outlook.com>
2023-11-20 00:52:25 -0600

    profile page

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -5,7 +5,10 @@ import Image from 'next/image';
 import {useSession} from 'next-auth/react';
 import { usePathname, useRouter } from 'next/navigation';
 
-const PromptCard = ({post, handleTagClick, hangleEdit, HandleDelete}) => {
+const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
+  const {data: session} = useSession();
+  const pathName = usePathname();
+  const router = useRouter();
   const [copied, setCopied] = useState("");
 
   const handleCopy = () => {
@@ -51,6 +54,22 @@ const PromptCard = ({post, handleTagClick, hangleEdit, HandleDelete}) => {
         >
           {post.tag}
         </p>
+
+        {session?.user.id === post.creator._id && 
+        pathName === '/profile' && (
+          <div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
+            <p
+              className='font-inter text-sm green_gradient cursor-pointer' onClick={handleEdit}
+            >
+              Edit
+            </p>
+            <p
+              className='font-inter text-sm orange_gradient cursor-pointer' onClick={handleDelete}
+            >
+              Delete
+            </p>
+          </div>
+        )}
     </div>
   )
 }
